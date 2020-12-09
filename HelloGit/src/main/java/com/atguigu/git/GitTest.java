f8d8972cbba509755048eef51ffce430a1fe3176
xinxiaxindong <1791158087@qq.com>
2020-12-09 22:17:14 +0800

    更新1

---
+++ HelloGit/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author xinxiaxindong
+ * @create 2020-12-09 22:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("更新1");
+    }
+}
